323ac09cab2948b625148835710cd24f48970333
Fredrik Schiller <fredrik.schiller@1337.tech>
2023-05-02 09:04:17 +0200

    Constructors are now generated. This made some code superflous. Immutability is now handled correctly.

--- PocoBuilder.Tests/Builder.cs
+++ PocoBuilder.Tests/Builder.cs
@@ -3,75 +3,131 @@
     [TestClass]
     public class Builder
     {
-        public interface IModel
+        // PocoBuilder takes an interface and builds a proper class.
+        // This leverages the power of combining several interfaces
+        // to form a contract, without forcing the boiler-plate
+        // of actually implementing all those interfaces.
+
+        // Consider the following interfaces as a model description
+        public interface IBaseModel
         {
             int Id { get; }
             string Name { get; set; }
         }
-        public interface IPeripheral1 : IModel
+        public interface IPeripheral1 : IBaseModel
         {
             string Data1 { get; set; }
         }
-        public interface IPeripheral2 : IModel
+        public interface IPeripheral2 : IBaseModel
         {
-            string Data2 { get; }
+            Guid? Data2 { get; }
         }
 
-        public interface ITest1 : IModel, IPeripheral1, IPeripheral2 { }
+        // They can then contextually be combined to represent a contract.
+        // When testing, it is apropriate to use all of them, but when e.g.
+        // writing a translation service for products, you would not need to include the prices
+        // even though both descriptions and prices should be defined and known in a core assembly.
+        public interface ITest1 : IBaseModel, IPeripheral1, IPeripheral2 { }
         [TestMethod]
-        public void Test1_MutableProperties()
+        public void Test1_Initialization()
         {
-            Assert.IsTrue(PocoBuilder.VerifyPocoInterface<ITest1>());
+            // To build (or fetch from memory cache) the poco type.   
+            var interfaceAsClassType = PocoBuilder.GetTypeFor<ITest1>();
+            
+            // The new type has both an empty constructor
+            var emptyInstance = Activator.CreateInstance(interfaceAsClassType) as ITest1;
+            Assert.IsNotNull(emptyInstance);
+            Assert.IsNull(emptyInstance.Data1);
 
-            var instance = PocoBuilder.CreatePocoInstance<ITest1>(out var _);
-            Assert.IsNotNull(instance);
-            Assert.IsInstanceOfType(instance, typeof(ITest1));
+            // and a constructor for assigning values, to support serialization.
+            var filledInstance = Activator.CreateInstance(interfaceAsClassType, 1, "Name", "Data1", Guid.NewGuid()) as ITest1;
+            Assert.IsNotNull(filledInstance);
+            Assert.IsNotNull(filledInstance.Data1);
 
-            instance.Name = "Test";
-            Assert.AreEqual("Test", instance.Name);
+            // It also creates all the properties, with getters and setters as defined by the interface.
+            emptyInstance.Data1 = "This property just mutated...";
+            Assert.IsNotNull(emptyInstance.Data1);
 
-            instance.Data1 = "Arbitrary information about the model";
-            Assert.IsNotNull(instance.Data1);
+            // Immutable properties can of course only be read.
+            var data2 = filledInstance.Data2;
+            Assert.IsNotNull(data2);
         }
 
-        public interface IUnrelated1 { string This { get; } }
-        public interface IUnrelated2 { string That { get; } }
-        public interface ITest2<T> : IModel, IUnrelated1, IUnrelated2
-        {
-            T Value { get; }
-        }
         [TestMethod]
-        public void Test2_ImmutableProperties()
+        public void Test2_TypeSafeInitialization()
         {
-            Assert.IsTrue(PocoBuilder.VerifyPocoInterface<ITest2<int>>());
-
-            var instance = PocoBuilder.CreatePocoInstance<ITest2<int>>(out var fields);
-            Assert.IsNotNull(instance);
-            Assert.IsInstanceOfType(instance, typeof(ITest2<int>));
-            Assert.IsInstanceOfType(instance, typeof(IUnrelated1));
-            Assert.IsInstanceOfType(instance, typeof(IUnrelated2));
-
-            fields[nameof(instance.Id)].SetValue(instance, 12345);
-            fields[nameof(instance.Value)].SetValue(instance, 6);
-            fields[nameof(instance.Name)].SetValue(instance, "Test");
-            fields[nameof(instance.This)].SetValue(instance, "Arbitrary information about the model");
-            fields[nameof(instance.That)].SetValue(instance, "Arbitrary information about the model");
-
-            Assert.AreEqual(12345, instance.Id);
-            Assert.AreEqual("Test", instance.Name);
-            Assert.IsTrue(instance.Value == 6);
-            Assert.IsNotNull(instance.This);
-            Assert.IsNotNull(instance.That);
+            // Using Activator to pass the initial values as an array of objects is typically a bit unsafe.
+            // PocoBuilder therefore offers this alternative, to get a type-safe activation.
+            var filledInstance = PocoBuilder.CreateInstanceOf<ITest1>(init => init
+                .Set(i => i.Id, 1)
+                .Set(i => i.Name, "The name of the thingy")
+                .Set(i => i.Data1, "Something else about the thingy")
+                .Set(i => i.Data2, Guid.NewGuid())
+            );
+            // This technique can also be used to map foreign properties,
+            // or expanded to take into account several data sources.
+            Assert.IsNotNull(filledInstance);
+            Assert.AreEqual(1, filledInstance.Id);
+            Assert.IsNotNull(filledInstance.Data2);
         }
 
-        public interface IUnrelated3 { string That { get; } }
-        public interface IConflictingProperties : IUnrelated1, IUnrelated2, IUnrelated3 { }
-        public interface IMethod { void DoSomething(); }
+        // The interfaces the poco builder likes has some limitations.
+        // They may not have conflicting properties.
+        public interface IUnrelated1 { string Data1 { get; } }
+        public interface IConflictingProperties : IPeripheral1, IUnrelated1 { }
+        
+        // Nor can they contain methods, even with default implementations.
+        // While a type will be created, the method will not be carried over!
+        public interface IContainsMethod { void DoSomething(); }
         [TestMethod]
         public void Test3_InvalidInterfaceDeclarations()
         {
+            // Unrelated interfaces can serve a purpose, like ITableEntity, but
+            // make sure no properties get duplicated!
             Assert.IsFalse(PocoBuilder.VerifyPocoInterface<IConflictingProperties>());
-            Assert.IsFalse(PocoBuilder.VerifyPocoInterface<IMethod>());
+            // (The IDE should report this problem as well, when these properties are referenced)
+
+            // Methods are also not allowed - the poco builder is not a compiler!
+            Assert.IsFalse(PocoBuilder.VerifyPocoInterface<IContainsMethod>());
+            // Instead, use base classes and/or extension methods.
+
+        }
+
+        // Base classes can be useful, especially when inheriting from
+        // data-source specific entities, like TableEntity.
+        // But extension methods are also a great way to extend the functionality
+        // of the interfaces!
+        public abstract class GenericBase
+        {
+            // No need to implement the interface properties
+            public ITest1 Model => (ITest1)this;
+            
+            // Public parameter-less constructors are called when the constructed type is instantiated.
+            public bool DefaultContructorCalled { get; } = true;
+            
+            // Custom contstructors are never called by the PocoBuilder
+            public bool CustomConstructorCalled { get; } = false;
+
+            public GenericBase() { DefaultContructorCalled = true; }
+            public GenericBase(ITest1 _) {  CustomConstructorCalled = true; }
+            public bool NameIsSet => !string.IsNullOrEmpty(Model.Name);
+        }
+
+        [TestMethod]
+        public void Test4_BaseClass()
+        {
+            // The builder returns two references to the same instance.
+            var (asInterface, asParent) = PocoBuilder.CreateInstanceOf<ITest1, GenericBase>();
+            Assert.AreEqual((object)asParent, (object)asInterface);
+            Assert.IsInstanceOfType(asInterface, typeof(ITest1));
+            Assert.IsInstanceOfType(asParent, typeof(GenericBase));
+
+            Assert.IsTrue(asParent.DefaultContructorCalled);
+            Assert.IsFalse(asParent.CustomConstructorCalled);
+
+            Assert.IsFalse(asParent.NameIsSet);
+            asInterface.Name = "This is a name";
+            Assert.IsTrue(asParent.NameIsSet);
         }
     }
 }
---
+++ PocoBuilder/PocoBuilder.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Concurrent;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace PocoBuilder
+{
+    public class PocoBuilder
+    {
+        public interface ISetter<TInterface> { ISetter<TInterface> Set<TValue>(Expression<Func<TInterface, TValue>> property, TValue value); }
+        readonly struct Setter<TInterface> : ISetter<TInterface>
+        {
+            readonly Dictionary<string, object?> values;
+            public Setter() => values = Properties<TInterface>().ToDictionary(m => m.Name, m => (object?)null);
+            public ISetter<TInterface> Set<TValue>(Expression<Func<TInterface, TValue>> property, TValue value)
+            {
+                if (property.Body is MemberExpression expression)
+                {
+                    var memberName = expression.Member.Name;
+                    if (value != null && values.ContainsKey(memberName))
+                        values[expression.Member.Name] = value;
+                }
+                return this;
+            }
+            public static implicit operator object?[]?(Setter<TInterface> setter) => setter.values.Values.ToArray();
+        }
+
+        readonly static AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new("DynamicTypes"), AssemblyBuilderAccess.Run);
+        readonly static ModuleBuilder module = assembly.DefineDynamicModule("DynamicModule");
+
+        readonly static ConcurrentDictionary<Type, HashSet<PropertyInfo>> propertyCache = new();
+        static HashSet<PropertyInfo> Properties<TInterface>() => Properties(typeof(TInterface));
+        static HashSet<PropertyInfo> Properties(Type interfaceType) => propertyCache.GetOrAdd(interfaceType, interfaceType =>
+        {
+            var declaredProperties = interfaceType.GetProperties();
+            var inheritedProperties = interfaceType.GetInterfaces().SelectMany(i => i.GetProperties());
+            return new HashSet<PropertyInfo>(declaredProperties.Union(inheritedProperties));
+        });
+
+        public static Type GetTypeFor<TInterface>() => GetTypeFor<TInterface, object>();
+        public static Type GetTypeFor<TInterface, TParent>()
+        {
+            var interfaceType = typeof(TInterface); var parentType = typeof(TParent);
+            var typeName = $"{parentType.Name}.{interfaceType.Name}";
+
+            Type typeBuilder()
+            {
+                var newType = module.DefineType(typeName, TypeAttributes.Public, parentType);
+                var constructor = parentType?.GetConstructor(BindingFlags.Instance | BindingFlags.Public, Array.Empty<Type>());
+                return ImplementClass(newType, interfaceType, constructor);
+            }
+            return module.GetType(typeName) ?? typeBuilder();
+        }
+
+        public static TInterface CreateInstanceOf<TInterface>(Action<ISetter<TInterface>>? initializer = null)
+            => (TInterface)GetInstanceOf(GetTypeFor<TInterface>(), initializer);
+        public static (TInterface asInterface, TParent asParent) CreateInstanceOf<TInterface, TParent>(Action<ISetter<TInterface>>? initializer = null)
+        {
+            var type = GetTypeFor<TInterface, TParent>();
+            object instance = GetInstanceOf(type, initializer);
+            return ((TInterface)instance, (TParent)instance);
+        }
+        static object GetInstanceOf<TInterface>(Type type, Action<ISetter<TInterface>>? initializer = null)
+        {
+            object instance;
+            object?[]? parameters = null;
+
+            if (initializer != null)
+            {
+                var setter = new Setter<TInterface>(); initializer(setter);
+                parameters = setter;
+            }
+
+            instance = Activator.CreateInstance(type, parameters) ?? throw new InvalidOperationException();
+            return instance;
+        }
+        
+        public static bool VerifyPocoInterface<TInterface>()
+        {
+            var interfaceType = typeof(TInterface);
+            if (!interfaceType.IsInterface) return false;
+
+            var declaredProperties = interfaceType.GetProperties();
+            var inheritedProperties = interfaceType.GetInterfaces().SelectMany(i => i.GetProperties());
+            var allPropertyNames = declaredProperties.Union(inheritedProperties).Select(p => p.Name);
+            if (allPropertyNames.Count() != allPropertyNames.Distinct().Count()) return false;
+
+            var declaredMethods = interfaceType.GetMethods();
+            var inheritedMethods = interfaceType.GetInterfaces().SelectMany(i => i.GetMethods());
+            var allMethods = declaredMethods.Union(inheritedMethods).ToArray();
+            if (allMethods.Where(m => (m.Attributes & MethodAttributes.SpecialName) == 0).Any()) return false;
+
+            return true;
+        }
+
+        static Type ImplementClass(TypeBuilder type, Type interfaceType, ConstructorInfo? parentConstructor = null)
+        {
+            type.AddInterfaceImplementation(interfaceType);
+            var properties = Properties(interfaceType);
+
+            var ctorIL = type.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, properties.Select(p => p.PropertyType).ToArray()).GetILGenerator();
+            foreach ((var property, var index) in properties.Select((p, i) => (p, i + 1)))
+            {
+                ctorIL.Emit(OpCodes.Ldarg_0);
+                switch (index)
+                {
+                    case 1: ctorIL.Emit(OpCodes.Ldarg_1); break;
+                    case 2: ctorIL.Emit(OpCodes.Ldarg_2); break;
+                    case 3: ctorIL.Emit(OpCodes.Ldarg_3); break;
+                    default: ctorIL.Emit(OpCodes.Ldarg_S, index); break;
+                }
+                ctorIL.Emit(OpCodes.Stfld, ImplementProperty(property, type));
+            }
+            if (parentConstructor != null)
+            {
+                type.DefineDefaultConstructor(MethodAttributes.Public);
+                ctorIL.Emit(OpCodes.Ldarg_0);
+                ctorIL.Emit(OpCodes.Call, parentConstructor);
+            }
+            ctorIL.Emit(OpCodes.Ret);
+            return type.CreateType() ?? throw new InvalidOperationException();
+        }
+        static FieldBuilder ImplementProperty(PropertyInfo propertyInfo, TypeBuilder type)
+        {
+            FieldBuilder field = type.DefineField("__" + propertyInfo.Name, propertyInfo.PropertyType, FieldAttributes.Private);
+            PropertyBuilder property = type.DefineProperty(propertyInfo.Name, PropertyAttributes.None, propertyInfo.PropertyType, null);
+
+            MethodAttributes attributes =
+                MethodAttributes.Public |
+                MethodAttributes.HideBySig |
+                MethodAttributes.SpecialName |
+                MethodAttributes.Virtual;
+
+            var interfaceGetter = propertyInfo.GetGetMethod();
+            if (interfaceGetter != null)
+            {
+                MethodBuilder getter = type.DefineMethod("get_" + propertyInfo.Name, attributes, propertyInfo.PropertyType, Type.EmptyTypes);
+                ILGenerator getIL = getter.GetILGenerator();
+                getIL.Emit(OpCodes.Ldarg_0);
+                getIL.Emit(OpCodes.Ldfld, field);
+                getIL.Emit(OpCodes.Ret);
+                property.SetGetMethod(getter);
+                type.DefineMethodOverride(getter, interfaceGetter);
+            }
+
+            var interfaceSetter = propertyInfo.GetSetMethod();
+            if (interfaceSetter != null)
+            {
+                MethodBuilder setter = type.DefineMethod("set_" + propertyInfo.Name, attributes, null, new Type[] { propertyInfo.PropertyType });
+                ILGenerator setIL = setter.GetILGenerator();
+                setIL.Emit(OpCodes.Ldarg_0);
+                setIL.Emit(OpCodes.Ldarg_1);
+                setIL.Emit(OpCodes.Stfld, field);
+                setIL.Emit(OpCodes.Ret);
+                property.SetSetMethod(setter);
+                type.DefineMethodOverride(setter, interfaceSetter);
+            }
+            return field;
+        }
+    }
+}
