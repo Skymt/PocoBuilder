4c50260700c02420064c5ba8170b1cffa355b322
Fredrik Schiller <fredrik.schiller@tretton37.com>
2023-11-12 04:11:04 +0100

    I think converters won't be instantiated too often - registration in startup. I lessened the caching logics because of this.
    Also removed accidental using.

--- PocoBuilder.Tests/Serialization.cs
+++ PocoBuilder.Tests/Serialization.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json;
-using System.Transactions;
 
 namespace PocoBuilder.Tests;
 
--- PocoBuilder/DTOConverter.cs
+++ PocoBuilder/DTOConverter.cs
@@ -1,19 +1,24 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace PocoBuilder;
 public class DTOConverter<TInterface> : JsonConverter<TInterface>, IEqualityComparer<string>
 {
-    static readonly ConcurrentDictionary<Type, IEnumerable<KeyValuePair<string, Type>>> propertyTypeCache = new();
-    
+    static readonly Type targetType = DTOBuilder.GetTypeFor<TInterface>();
+    static readonly IEnumerable<KeyValuePair<string, Type>> propertyTypes;
+    static DTOConverter()
+    {
+        propertyTypes = targetType.GetProperties().Select(p => new KeyValuePair<string, Type>(p.Name, p.PropertyType));
+    }
     public override TInterface? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var jsonDoc = JsonDocument.ParseValue(ref reader);
-        var targetType = DTOBuilder.GetTypeFor<TInterface>();
-        var (types, values) = GetConstructorSignature(targetType, options.PropertyNameCaseInsensitive ? this : null);
+        IEqualityComparer<string>? comparer = options.PropertyNameCaseInsensitive ? this : null;
+        Dictionary<string, Type> types = new(propertyTypes, comparer);
+        Dictionary<string, object?> values = new(propertyTypes.Select(valueFactory), comparer);
+        static KeyValuePair<string, object?> valueFactory(KeyValuePair<string, Type> registration) => new(registration.Key, null);
 
+        var jsonDoc = JsonDocument.ParseValue(ref reader);
         foreach (var node in jsonDoc.RootElement.EnumerateObject())
             values[node.Name] = node.Value.Deserialize(types[node.Name], options);
 
@@ -23,18 +28,6 @@ public class DTOConverter<TInterface> : JsonConverter<TInterface>, IEqualityComp
     public override void Write(Utf8JsonWriter writer, TInterface value, JsonSerializerOptions options) =>
         JsonSerializer.Serialize(writer, Convert.ChangeType(value, DTOBuilder.GetTypeFor<TInterface>()), options);
 
-    static (Dictionary<string, Type>, Dictionary<string, object?>) GetConstructorSignature(Type targetType, IEqualityComparer<string>? comparer)
-    {
-        var propertyTypes = propertyTypeCache.GetOrAdd(targetType, t =>
-        {
-            var propertyTypes = t.GetProperties();
-            return propertyTypes.Select(t => new KeyValuePair<string, Type>(t.Name, t.PropertyType));
-        });
-        var types = new Dictionary<string, Type>(propertyTypes, comparer);
-        var values = new Dictionary<string, object?>(propertyTypes.Select(valueFactory), comparer);
-        return (types, values);
-        static KeyValuePair<string, object?> valueFactory(KeyValuePair<string, Type> registration) => new(registration.Key, null);
-    }
     bool IEqualityComparer<string>.Equals(string? x, string? y) => x?.ToLower() == y?.ToLower();
     int IEqualityComparer<string>.GetHashCode([DisallowNull] string obj) => obj.ToLower().GetHashCode();
 }
